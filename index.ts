8a01bc22753faa3c8a96d848a7d2fc867764ded4
Chris Vandenberg <chris@altruisticsoftware.com>
2020-04-04 18:37:32 -0500

    add docs and split functions

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+/**
+ * @description Default prefixes.
+ */
 interface ResponsiveObject {
   sm?: string;
   md?: string;
@@ -6,19 +9,45 @@ interface ResponsiveObject {
   xxl?: string;
 }
 
+/**
+ * @description Prefixes each chunked utility class.
+ */
 const iterator = (key: string) => (value: string): string => key + ':' + value;
 
-export const rclsx = (...args: [string, ResponsiveObject]): string => {
-  const [base = '', responsiveObject = {}] = args;
+/**
+ * @description Replaces all instances of more than one space with just one, and
+ * removes the outside whitespace.
+ */
+const format = (value: string): string => value.replace(/\s\s+/g, ' ').trim();
+
+/**
+ * @description Chunks each value associated with a breakpoint.
+ */
+const splitter = (value: string): string[] => format(value).split(' ');
+
+/**
+ * @description Merges all prefixed chunks into a single array.
+ */
+const reducer = (
+  accumulator: string[],
+  [key, value]: [string, string],
+): string[] => [...accumulator, ...splitter(value).map(iterator(key))];
+
+export const rclsx = (
+  ...args: [string | ResponsiveObject, ResponsiveObject?]
+): string => {
+  let [base = '', responsiveObject = {}] = args;
+
+  // If the first parameter is an object, we can assume there is no base class,
+  // and that this is intended to be the responsive object.
+  if (typeof base === 'object') {
+    responsiveObject = base;
+    base = '';
+  }
 
   const entries: Array<[string, string]> = Object.entries(responsiveObject);
 
-  const result: string[] = entries.reduce(
-    (acc, [key, value]) => [...acc, ...value.split(' ').map(iterator(key))],
-    [base],
-  );
+  const combined: string[] = entries.reduce(reducer, [format(base)]);
 
-  return result.join(' ');
+  return combined.join(' ');
 };
-
-console.log(rclsx('lol', { sm: 'px-1 py-1', md: 'px-2 py-2' }));
